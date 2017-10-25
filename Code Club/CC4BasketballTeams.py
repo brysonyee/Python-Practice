118f9a567e8ed338d9e94367c144833d25d57c47
brysonyee <33079823+brysonyee@users.noreply.github.com>
2017-10-25 16:27:29 -0700

    Added Andreas

--- Code Club/CC4BasketballTeams.py
+++ Code Club/CC4BasketballTeams.py
@@ -5,15 +5,20 @@ Created on Oct 25, 2017
 '''
 import random
 
-players = ['Ali','Mike','Sean','Rachel','Jeonghwan','Bryson']
+players = ['Ali','Mike','Sean','Rachel','Jeonghwan','Bryson', 'Andreas']
 
 print('Current Squad:')
-for i in range(6):
+for i in range(7):
     print(players[i]) 
 print(' ')
 
+players.remove('Andreas')
+
 teamA = [] 
 teamB = []
+
+print('Team Losers Who Don\'t Play Basketball')
+print('[\'Andreas\']')
 
 while len(players)>0:
     playerA = random.choice(players)
